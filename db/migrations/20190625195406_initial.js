4ee14433e414d2b79f41ccddd413ae83a2f31e33
Jarrett Kong <jarrettkong@me.com>
2019-06-26 11:37:50 -0600

    Reset id counters

--- db/migrations/20190625195406_initial.js
+++ db/migrations/20190625195406_initial.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
 	return Promise.all([
 		knex.schema.createTable('pokemon', table => {
 			table.increments('id').primary();
@@ -29,7 +29,7 @@ exports.up = function(knex, Promise) {
 	]);
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
 	return Promise.all([
 		knex.schema.dropTable('trainers_pokemon'),
 		knex.schema.dropTable('trainers'),
--- db/seeds/dev/pokemon.js
+++ db/seeds/dev/pokemon.js
@@ -1,10 +1,14 @@
 const pokemonData = require('../../../pokemon');
 const trainerData = require('../../../trainers');
 
-exports.seed = function(knex, Promise) {
+exports.seed = function(knex) {
 	return knex('pokemon')
 		.del()
-		.then(() => knex('trainers').del())
+		.then(async () => {
+			await knex('trainers').del();
+			await knex.raw('TRUNCATE TABLE pokemon RESTART IDENTITY CASCADE');
+			await knex.raw('TRUNCATE TABLE trainers RESTART IDENTITY CASCADE');
+		})
 		.then(() => {
 			return Promise.all([knex('pokemon').insert(pokemonData)]);
 		})
