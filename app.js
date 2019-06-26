8db6dc6f7296f2b87064cb7e60e0fda4c2758186
Jarrett Kong <jarrettkong@me.com>
2019-06-25 20:47:45 -0600

    Add get and post endpoints

--- app.js
+++ app.js
@@ -14,4 +14,63 @@ app.get('/api/v1/pokemon', (req, res) => {
 		.catch(error => res.status(500).json({ error }));
 });
 
+app.post('/api/v1/pokemon', (req, res) => {
+	const pokemon = req.body;
+	const required = ['id', 'name', 'type', 'HP', 'Attack', 'Defense', 'Sp. Attack', 'Sp. Defense', 'Speed'];
+
+	for (let param of required) {
+		if (!pokemon[param]) {
+			return res.status(422).send('Unprocessible Entity');
+		}
+	}
+
+	db('pokemon')
+		.insert(pokemon, 'id')
+		.then(pokemon => res.status(201).send({ id: pokemon[0] }))
+		.catch(error => res.status(500).send({ error }));
+});
+
+app.get('/api/v1/trainers', (req, res) => {
+	db('trainers')
+		.select()
+		.then(trainers => res.status(200).json(trainers))
+		.catch(error => res.status(500).json({ error }));
+});
+
+app.post('/api/v1/trainers', (req, res) => {
+	const trainer = req.body;
+	const required = ['id', 'name', 'pokemon'];
+
+	for (let param of required) {
+		if (!trainer[param]) {
+			return res.status(422).send('Unprocessible Entity');
+		}
+	}
+
+	if (trainer.pokemon.length < 1 || trainer.pokemon.length > 6) {
+		return res.status(422).send('Unprocessible Entity');
+	}
+
+	db('trainers')
+		.insert(trainer, 'id')
+		.then(trainer => res.status(201).send({ id: trainer[0] }))
+		.catch(error => res.status(500).send({ error }));
+});
+
+app.get('/api/v1/pokemon/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	db('pokemon')
+		.where('id', id)
+		.then(pokemon => res.status(200).json(pokemon))
+		.catch(() => res.status(404).send(`No pokemon found with number ${id}`));
+});
+
+app.get('/api/v1/trainers/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	db('trainers')
+		.where('id', id)
+		.then(trainer => res.status(200).json(trainer))
+		.catch(() => res.status(404).send(`No trainer found with id ${id}`));
+});
+
 app.listen(port, console.log(`Listening on port ${port}.`));
