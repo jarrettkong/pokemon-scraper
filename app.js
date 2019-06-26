11a07d4087c188f08501e30af291c61f60445886
Jarrett Kong <jarrettkong@me.com>
2019-06-25 20:14:09 -0600

    Add pokemon get endpoint

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const db = require('knex')(configuration);
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 3001
+const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
+app.get('/api/v1/pokemon', (req, res) => {
+	db('pokemon')
+		.select()
+		.then(pokemon => res.status(200).json(pokemon))
+		.catch(error => res.status(500).json({ error }));
+});
 
-
-app.listen(port, console.log(`Listening on port ${port}.`))
+app.listen(port, console.log(`Listening on port ${port}.`));
