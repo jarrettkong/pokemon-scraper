ef6aee4c1d8c73a5a352841812d0f6f1fa24b42e
Jarrett Kong <jarrettkong@me.com>
2019-06-26 09:51:36 -0600

    Add seed file

--- db/migrations/20190625195406_initial.js
+++ db/migrations/20190625195406_initial.js
@@ -4,12 +4,22 @@ exports.up = function(knex, Promise) {
 			table.increments('id').primary();
 			table.string('name');
 			table.json('type');
-			table.number('HP');
-			table.number('Attack');
-			table.number('Defense');
-			table.number('Sp. Attack');
-			table.number('Sp. Defense');
-			table.number('Speed');
+			table.integer('HP');
+			table.integer('Attack');
+			table.integer('Defense');
+			table.integer('Sp Attack');
+			table.integer('Sp Defense');
+			table.integer('Speed');
+		}),
+		knex.schema.createTable('trainers_pokemon', table => {
+			table
+				.integer('pokemon_id')
+				.unsigned()
+				.references('pokemon.id');
+			table
+				.integer('trainer_id')
+				.unsigned()
+				.references('trainers.id');
 		}),
 		knex.schema.createTable('trainers', table => {
 			table.increments('id').primary();
--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: 'postgress://localhost/pokemon',
-    migrations: {
-      directory: './db/migrations'
-    }
-  },
-
+	development: {
+		client: 'pg',
+		connection: 'postgress://localhost/pokemon',
+		migrations: {
+			directory: './db/migrations'
+		},
+		seeds: {
+			directory: './db/seeds/dev'
+		},
+		useNullAsDefault: true
+	}
 };
