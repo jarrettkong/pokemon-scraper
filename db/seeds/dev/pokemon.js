c037d4c67c6cdeafe3a6596389adf2110a1d4a4d
Jarrett Kong <jarrettkong@me.com>
2019-06-26 11:17:57 -0600

    Change seed file to use promise all

--- db/seeds/dev/pokemon.js
+++ db/seeds/dev/pokemon.js
@@ -4,5 +4,13 @@ const trainerData = require('../../../trainers');
 exports.seed = function(knex, Promise) {
 	return knex('pokemon')
 		.del()
-		.insert(pokemonData);
+		.then(() => knex('trainers').del())
+		.then(() => {
+			return Promise.all([knex('pokemon').insert(pokemonData)]);
+		})
+		.then(() => {
+			return Promise.all([knex('trainers').insert(trainerData)]);
+		})
+		.then(() => console.log('Seeding Complete'))
+		.catch(() => console.log('Error seeding file'));
 };
